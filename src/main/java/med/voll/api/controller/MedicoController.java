1ea3788c303e6b80a9381fc6aa90ed8d3ee9a572
matheus mozart <mmsnborges@gmail.com>
2024-01-21 12:20:25 -0400

    paginação com spring

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,9 @@ import med.voll.api.medico.DadosLitagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,20 +18,17 @@ import java.util.List;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-@Autowired
-private MedicoRepository repository;
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
     @Transactional
-    public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados){
+    public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
     @GetMapping
-    public List<DadosLitagemMedico> listar(){
-    return repository.findAll().stream()
-            .map(DadosLitagemMedico::new)
-            .toList()
-            ;
+    public Page<DadosLitagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosLitagemMedico::new);
     }
 }
