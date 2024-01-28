38ee44e7d8b38c8db2fe8ef6f5727e1bdbce5718
matheus mozart <mmsnborges@gmail.com>
2024-01-28 13:45:39 -0400

    recuperando token do header

--- src/main/java/med/voll/api/infra/security/SecutirityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecutirityFilter.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.header.Header;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,8 +14,19 @@ public class SecutirityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT  = recuperarToken(request);
+
+        System.out.println(tokenJWT);
         //chamando próximo filtro
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token ausente no cabeçalho authorizantion");
+        }
+        return authorizationHeader.replace("Bearer ", "");
     }
 
 
