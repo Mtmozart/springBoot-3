62f3dc92e6ef7a88827f5f34b1e6206ee6977e7c
matheus mozart <mmsnborges@gmail.com>
2024-01-29 18:29:51 -0400

    terminado

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -13,7 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 //Componente genérico
 @Component
-public class SecutirityFilter extends OncePerRequestFilter {
+public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
