8ffff28da5bd2b17069d30909d251af1b9757f51
matheus mozart <mmsnborges@gmail.com>
2024-01-30 19:20:49 -0400

    falta a achar o erro da query

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,29 @@ public class AgendaDeConsultas {
     PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentosConsulta dados){
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        if (!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("O paciente informado não existe.");
+        }
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("O médico informado não existe.");
+        }
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+
+        var medico = escolherMedico(dados);
+
+
 
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentosConsulta dados) {
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("A especilidade é obrigatório quando não há méidco escolhido");
+        }
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 }
--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,4 +3,6 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+
 }
